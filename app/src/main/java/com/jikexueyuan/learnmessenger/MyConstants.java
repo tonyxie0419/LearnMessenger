7a99805d34eea23a4b5a6124ad76a2a6471b0ef2
tonyxie0419 <tonyxie_0419@qq.com>
2017-06-22 13:17:16 +0800

    Initial commit

---
+++ app/src/main/java/com/jikexueyuan/learnmessenger/MyConstants.java
@@ -0,0 +1,11 @@
+package com.jikexueyuan.learnmessenger;
+
+/**
+ * Created by admin on 2017/6/22.
+ */
+
+class MyConstants {
+
+    static final int MSG_FROM_CLIENT = 10086;
+    static final int MSG_FROM_SERVICE = 10010;
+}
